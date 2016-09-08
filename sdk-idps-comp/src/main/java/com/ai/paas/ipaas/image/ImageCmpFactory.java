7bea2add4443d4e2e81a617dc07dbd7221a16428
DOUXF <douxf@asiainfo.com>
2016-09-08 05:32:19 +0200

    fix build.gradle

--- sdk-idps-comp/src/main/java/com/ai/paas/ipaas/image/ImageCmpFactory.java
+++ sdk-idps-comp/src/main/java/com/ai/paas/ipaas/image/ImageCmpFactory.java
@@ -4,21 +4,38 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.ai.paas.ipaas.image.impl.ImageClientImpl;
+import com.ai.paas.ipaas.util.Assert;
 
 public class ImageCmpFactory {
-	private String imageUrl = null;
 	private static Map<String, IImageClient> imageClients = new ConcurrentHashMap<String, IImageClient>();
-	
-	/**
-	 * @param imageUrl "http://10.1.235.199:8086/iPaas-IDPS"
-	 */
-	public ImageCmpFactory(String imageUrl) {
-		this.imageUrl = imageUrl;
+
+	private ImageCmpFactory(String imageUrl) {
 	}
 
-	public IImageClient getClient() throws Exception {
+	/**
+	 * 获取图片客户端
+	 * 
+	 * @param interUrl
+	 *            图片对外服务地址，可以是nginx地址或者域名加地址 http://xxx.xxx.xxx.xxx:port/image
+	 * @param intraUrl
+	 * @return
+	 * @throws Exception
+	 */
+	public static IImageClient getClient(String interUrl, String intraUrl)
+			throws Exception {
 		IImageClient iImageClient = null;
-		iImageClient = new ImageClientImpl("false", "", "", "", imageUrl, imageUrl);
+		Assert.notNull(interUrl, "the internet address can not null!");
+		Assert.notNull(intraUrl, "the intranet address can not null!");
+		if (interUrl.endsWith("/"))
+			interUrl = interUrl.substring(0, interUrl.lastIndexOf("/"));
+		if (intraUrl.endsWith("/"))
+			intraUrl = intraUrl.substring(0, interUrl.lastIndexOf("/"));
+
+		// 判断一下是否有
+		if (null != imageClients.get(interUrl))
+			return imageClients.get(interUrl);
+		iImageClient = new ImageClientImpl("false", "", "", "", intraUrl,
+				interUrl);
 		imageClients.put("ImageCmpClient", iImageClient);
 		return iImageClient;
 	}
--- sdk-idps-comp/src/test/java/test/com/ai/paas/ipaas/idps/ImageTest.java
+++ sdk-idps-comp/src/test/java/test/com/ai/paas/ipaas/idps/ImageTest.java
@@ -12,8 +12,8 @@ import com.ai.paas.ipaas.image.IImageClient;
 import com.ai.paas.ipaas.image.ImageCmpFactory;
 
 public class ImageTest {
-	private static String imageUrl = "http://127.0.0.1:8080/idps-web";
-	
+	private static String imageUrl = "http://127.0.0.1:8080/idps-web/";
+
 	@Test
 	public void uploadImageTest() {
 		byte[] buffer = null;
@@ -34,52 +34,52 @@ public class ImageTest {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		try {
-			ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
-			IImageClient im = factory.getClient();
+			IImageClient im = ImageCmpFactory.getClient(imageUrl, imageUrl);
 			String id = im.upLoadImage(buffer, "666.jpg");
-			System.out.println("========= " +id + "==========");
-		}catch(Exception ex) {
+			System.out.println("========= " + id + "==========");
+		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 	}
 
-//	@Test
-//	public void getImageUrlTest() {
-//		try {
-//			ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
-//			IImageClient im = factory.getClient();
-//			System.out.println(im.getImageUrl("57ce9335b1c3aa54ce707ef8", ".jpg"));
-//			/**
-//			 * http://127.0.0.1:8080/idps-web/image/57c7c9deb1c3aa4a297e30a1.jpg?needAuth=false&mongoInfo={"mongoServer":"10.1.245.226:37037","database":"image","userName":"idps","password":"idps","bucket":"fs"}
-//			 */
-//		}catch(Exception ex) {
-//			ex.printStackTrace();
-//		}
-//	}
-	
-//	@Test
-//	public void deleteImageTest() {
-//		try {
-//			ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
-//			IImageClient im = factory.getClient();
-//			System.out.println(im.deleteImage("57ce9335b1c3aa54ce707ef8"));
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//		}
-//	}
-	
-//	@Test
-//	public void downloadTest() {
-//		try {
-//			ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
-//			IImageClient im = factory.getClient();
-//			im.getImage("57ce9335b1c3aa54ce707ef8", ".jpg", "");
-//			System.out.println(im.getImageUrl("57ce9335b1c3aa54ce707ef8", ".jpg"));
-//		} catch (Exception ex) {
-//			ex.printStackTrace();
-//		}
-//	}
+	// @Test
+	// public void getImageUrlTest() {
+	// try {
+	// ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
+	// IImageClient im = factory.getClient();
+	// System.out.println(im.getImageUrl("57ce9335b1c3aa54ce707ef8", ".jpg"));
+	// /**
+	// *
+	// http://127.0.0.1:8080/idps-web/image/57c7c9deb1c3aa4a297e30a1.jpg?needAuth=false&mongoInfo={"mongoServer":"10.1.245.226:37037","database":"image","userName":"idps","password":"idps","bucket":"fs"}
+	// */
+	// }catch(Exception ex) {
+	// ex.printStackTrace();
+	// }
+	// }
+
+	// @Test
+	// public void deleteImageTest() {
+	// try {
+	// ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
+	// IImageClient im = factory.getClient();
+	// System.out.println(im.deleteImage("57ce9335b1c3aa54ce707ef8"));
+	// } catch (Exception ex) {
+	// ex.printStackTrace();
+	// }
+	// }
+
+	// @Test
+	// public void downloadTest() {
+	// try {
+	// ImageCmpFactory factory = new ImageCmpFactory(imageUrl);
+	// IImageClient im = factory.getClient();
+	// im.getImage("57ce9335b1c3aa54ce707ef8", ".jpg", "");
+	// System.out.println(im.getImageUrl("57ce9335b1c3aa54ce707ef8", ".jpg"));
+	// } catch (Exception ex) {
+	// ex.printStackTrace();
+	// }
+	// }
 
 }
