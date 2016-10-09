4c1a668099e46bad238d08146ff8c298579f7c76
yinzf <woyinzf@163.com>
2016-10-09 16:12:39 +0800

    sunhz update

--- idps-web/src/main/java/com/ai/paas/ipaas/utils/LogListener.java
+++ idps-web/src/main/java/com/ai/paas/ipaas/utils/LogListener.java
@@ -8,7 +8,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
-
+import org.springframework.stereotype.Component;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 /** 
  * 基于注解的定时器 
  * @author sunhz 
